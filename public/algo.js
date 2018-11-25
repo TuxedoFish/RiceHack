3589e89aafffb5b9869383af98620376572ba9b5
TuxedoFish <harryliversedge@gmail.com>
2018-11-25 04:52:11 +0000

    hopefully seperated now

--- public/algo.js
+++ public/algo.js
@@ -22,14 +22,14 @@ function updateUI() {
 	$.getJSON(url, function(data) {
 		console.log("loaded : " + data["posts"].length + " data points")
 
-		$("#farmers main").empty();
+		$("#main").empty();
 
 	    for(var i=0; i<data["posts"].length; i++) {
 	    	var x = data["posts"][i];
 
 		    var markup = "<tr><td>" + x["name"] + "</td><td>" + x["cost"] + "</td><td>" + x["country"] + "</td>"
 		    				+ "<td>" + x["amount"] + "</td><td>" + x["shipping"] + "</td><td>" + x["quality"] + "</td></tr>";
-		    $("#farmers main").append(markup);
+		    $("#main").append(markup);
 	    }
 	});
 }
