e9b66dcd1b375a55f89fa9b03ce1502884942e3d
TuxedoFish <harryliversedge@gmail.com>
2018-11-25 01:18:00 +0000

    added some random data adding process

--- server.js
+++ server.js
@@ -2,18 +2,28 @@
 var express = require('express');
 var app = express();
 var admin = require('firebase-admin');
+var randomstring = require("randomstring");
 
 //Database
 var db;
+var NO_CUSTOMERS = 10;
+var NAMES = ["Susilo", "Albertus", "Abdurrahman", "Soetomo", "Boedino", "Ranomi"];
 //Start up firestore
 initFirebase();
 //Add a sample piece of info
-addDummyData();
+for(var i=0; i<NO_CUSTOMERS; i++) { addDummyData(); } 
 
 //Takes the user to the index page
 app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/data/getData", function(request, response) {
+  var state = randomstring.generate();
+  states[state] = moment();
+  var url = util.format('https://uclapi.com/oauth/authorise?client_id=%s&state=%s', client_id, state);
+  response.redirect(url);
 });
 
 function initFirebase() {
@@ -33,12 +43,16 @@ function initFirebase() {
 }
 
 function addDummyData() {
-	db.collection("STOCK").doc("2411183204").set(generateFakeData());
+	db.collection("STOCK").doc(randomstring.generate()).set(generateFakeData());
 }
 
 function generateFakeData() {
-	return {"name": "Fillipo",
-			"location": "Indonesia"};
+	return {"name": NAMES[Math.floor(Math.random() * 5)],
+			"country": "Indonesia",
+			"amount":  Math.floor(Math.random() * 100) + 100,
+			"quality": Math.floor(Math.random() * 5) + 1,
+			"shipping": Math.round(Math.random() * 100) / 10
+		};
 }
 
 //Listen for requests
